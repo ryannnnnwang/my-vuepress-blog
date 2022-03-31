f7a82f454177e99cd8d6dd80b10e209ff6eea001
ryannnnnwang <outtathewoods8@gmail.com>
2022-03-31 13:59:12 +0800

    second commit

---
+++ build/themeConfig.js
@@ -0,0 +1,34 @@
+const config = {
+	nav: [
+		{ text: "主页", link: "/" },
+		{
+			text: "ryannnnn的个人博客",
+			items: [
+				{ text: "Github", link: "https://github.com/ryannnnnwang" },
+				{
+					text: "掘金",
+					link: "https://juejin.cn/user/3087907771127559",
+				},
+			],
+		},
+	],
+	sidebar: [
+		{
+			title: "欢迎学习",
+			path: "/",
+			collapsable: false, // 不折叠
+			children: [{ title: "学前必读", path: "/" }],
+		},
+		{
+			title: "基础学习",
+			path: "/handbook/ConditionalTypes",
+			collapsable: false, // 不折叠
+			children: [
+				{ title: "条件类型", path: "/handbook/ConditionalTypes" },
+				{ title: "泛型", path: "/handbook/Generics" },
+			],
+		},
+	],
+}
+
+module.exports = config
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,7 @@
+const themeConfig = require("../../build/themeConfig")
+
 module.exports = {
 	title: "首页",
 	description: "playing around",
-	themeConfig: {
-		nav: [
-			{ text: "首页", link: "/" },
-			{
-				text: "ryannnnn的个人博客",
-				items: [
-					{ text: "Github", link: "https://github.com/ryannnnnwang" },
-					{
-						text: "掘金",
-						link: "https://juejin.cn/user/3087907771127559",
-					},
-				],
-			},
-		],
-		sidebar: [
-			{
-				title: "欢迎学习",
-				path: "/",
-				collapsable: false, // 不折叠
-				children: [{ title: "学前必读", path: "/" }],
-			},
-			{
-				title: "基础学习",
-				path: "/handbook/ConditionalTypes",
-				collapsable: false, // 不折叠
-				children: [
-					{ title: "条件类型", path: "/handbook/ConditionalTypes" },
-					{ title: "泛型", path: "/handbook/Generics" },
-				],
-			},
-		],
-	},
+	themeConfig,
 }
